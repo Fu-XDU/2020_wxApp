d7ff75e6b84e0f8ad5ecb9cfae062ca3e7aec1cb
付铭 <fuming.137@icloud.com>
2020-04-03 22:40:59 +0800

    add exp_order

---
+++ miniprogram/pages/functions/express/exp_orderSuccess/exp_orderSuccess.js
@@ -0,0 +1,9 @@
+// miniprogram/pages/functions/express/exp_orderSuccess/exp_orderSuccess.js
+const s=require('../../../main/main.js')
+Page({
+  ToMain: function () {
+    wx.redirectTo({
+      url: '../../../main/main'
+    })
+  },
+})
